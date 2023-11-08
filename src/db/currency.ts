41a8b8e38aa6bca3f4aca0c9c6d4982af9dd60d7
philipe lopes <philipe.lopes@greenant.com.br>
2023-11-08 00:57:34 -0300

    create more colums in currencies

--- src/db/currency.ts
+++ src/db/currency.ts
@@ -1,4 +1,8 @@
 /* Simulating DB */
-const currencies = [{ id: 1, label: "BRL", dolar: 5.10 }];
+const currencies = [
+  { id: 1, label: "BRL", dolar: 5.1 },
+  { id: 2, label: "EUR", dolar: 0.94 },
+  { id: 3, label: "USD", dolar: 1 },
+];
 
 export { currencies };
